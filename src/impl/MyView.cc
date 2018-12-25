9a9ba83aa1bd72ad36a0ab0d2498c187b3a9748d
chunlei.luo <chunlei.luo@goldenrivertek.com>
2018-12-25 20:25:11 +0800

    Imple show fake jgp.

--- src/impl/MyView.cc
+++ src/impl/MyView.cc
@@ -4,6 +4,11 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+#include <limits.h>
 
 void MyView::show()
 {
@@ -20,14 +25,16 @@ static const char * strings[] = {
 "      node2 -> node3 [label=\"edge_2_3\"];",
 "    }"};
 
-    char fn[] = "XXXXXX.dot";
-    int fd = mkstemps(fn, 4);
+    char *tmp_name = tempnam(NULL, NULL);
+    char dot_path[PATH_MAX];
+    sprintf(dot_path, "%s.dot", tmp_name);
+    char out_path[PATH_MAX];
+    sprintf(out_path, "%s.jpg", tmp_name);
+    free(tmp_name);
 
-    printf("create file name is %s", fn);
-
-    close(fd);
-    if (1)return;
-    FILE * file = fopen(fn, "w+");
+    printf("dot file is %s,\njpg file is %s\n", dot_path, out_path);
+    
+    FILE * file = fopen(dot_path, "w+");
 
     for(uint i=0; i<sizeof(strings)/sizeof(strings[0]); i++) {
         fwrite(strings[i], strlen(strings[i]), 1, file);
@@ -36,5 +43,18 @@ static const char * strings[] = {
     fclose(file);
 
 
-    //"dot -Tjpg test.dot -o test.jpg"
+	// execute the dot.
+    char command[PATH_MAX];
+    sprintf(command, "dot -Tjpg %s -o %s", dot_path, out_path);
+    int rc = system(command);
+    if (!rc) {
+		perror(NULL);
+	}
+	
+	sprintf(command, "eog %s", out_path);
+	
+	rc = system(command);
+    if (!rc) {
+		perror(NULL);
+	}
 }
